c630a00de12b580cf84e9bec9d03085c1f76caf0
Filippo <corti.filippo03@gmail.com>
2024-11-10 18:49:58 +0100

    Markers on the map

--- ReactNative/Lab04/map-app/components/MyMap.tsx
+++ ReactNative/Lab04/map-app/components/MyMap.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function MyMap() {
+    const [location, setLocation] = useState({ latitude: 37.78825, longitude: -122.4324 });
+
+    useEffect(() => {
+        const getLocationPermission = async () => {
+            // Request location permissions
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            if (status === 'granted') {
+                // Get the current location
+                try {
+                    const currentLocation = await Location.getCurrentPositionAsync({});
+                    setLocation(currentLocation.coords);
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                console.log("Location permission not granted");
+            }
+        };
+
+        getLocationPermission();
+    }, []);
+
     return (
         <View style={styles.container} >
             <MapView
                 style={styles.map}
                 showsUserLocation={true}
-                
-            />
+                initialRegion={{
+                    latitude: location?.latitude,
+                    longitude: location?.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}>
+                <Marker coordinate={{ latitude: 37.78825, longitude: -122.4324 }}>
+                </Marker>
+            </MapView>
         </View >
     );
 }
