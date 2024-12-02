587f6c4c208340b7fd1d8148b4ee8d240bab2ca1
Filippo <corti.filippo03@gmail.com>
2024-12-02 18:46:24 +0100

    Lab04 init (da casa fortunatamente)

---
+++ Android/Lab04/navigationapp/app/src/main/java/com/example/navigation_app/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.navigation_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigation_app.ui.theme.NavigationappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    Text(text = "Hello, Android")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApp()
+}
